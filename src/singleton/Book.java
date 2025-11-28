3fcd8c7dd40d7eb0727f6dcbf071fe57d67e4ec8
Oveis <oveishasanbaizid1227@gmail.com>
2025-11-28 23:36:29 +0330

    refactor "Singleton" Pattern

---
+++ src/singleton/Book.java
@@ -0,0 +1,17 @@
+package singleton;
+
+public class Book {
+
+    private static Book book;
+
+    private Book() {
+
+    }
+
+    public static Book getInstance() {
+        if (book == null) {
+            book = new Book();
+        }
+        return book;
+    }
+}
--- src/singleton/Main.java
+++ src/singleton/Main.java
@@ -1,6 +1,7 @@
-package Singleton;
+package singleton;
 
 public class Main {
+
     public static void main(String[] args) {
         Book book1 = Book.getInstance();
         Book book2 = Book.getInstance();
