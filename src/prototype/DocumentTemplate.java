00bdfe928aab2dc7b0c3ff22a3c176eb647813ed
Oveis <oveishasanbaizid1227@gmail.com>
2025-11-29 22:45:09 +0330

    add "Prototype" Pattern

---
+++ src/prototype/DocumentTemplate.java
@@ -0,0 +1,32 @@
+package prototype;
+
+public class DocumentTemplate {
+
+    private String title;
+    private String footer;
+    private int version;
+
+    public DocumentTemplate(String title, String footer, int version) {
+        this.title = title;
+        this.footer = footer;
+        this.version = version;
+    }
+
+    // prototype pattern
+    public DocumentTemplate cloneTemplate() {
+        return new DocumentTemplate(title, footer, version);
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "DocumentTemplate{" +
+                "title='" + title + '\'' +
+                ", footer='" + footer + '\'' +
+                ", version=" + version +
+                '}';
+    }
+}
