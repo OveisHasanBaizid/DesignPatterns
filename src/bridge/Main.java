8f16892fc705a58830d6458113363110050cd31e
Oveis <oveishasanbaizid1227@gmail.com>
2025-11-29 23:18:19 +0330

    add "Bridge" Pattern

---
+++ src/bridge/Main.java
@@ -0,0 +1,13 @@
+package bridge;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Device tv = new TV();
+        RemoteControl remote = new RemoteControl(tv);
+
+        remote.powerOn();
+        remote.changeVolume(50);
+        remote.powerOff();
+    }
+}
