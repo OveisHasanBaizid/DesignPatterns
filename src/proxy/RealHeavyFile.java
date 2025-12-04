38d861898014100ccb29317eddad2695d9f1433d
Oveis <oveishasanbaizid1227@gmail.com>
2025-12-05 00:08:21 +0330

    refactor "Proxy" Pattern

---
+++ src/proxy/RealHeavyFile.java
@@ -0,0 +1,16 @@
+package proxy;
+
+public class RealHeavyFile implements HeavyFile {
+
+    private final String path;
+
+    public RealHeavyFile(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public String load() {
+        System.out.println("Loading file from disk: " + path);
+        return "FileData(" + path + ")";
+    }
+}
